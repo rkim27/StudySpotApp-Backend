037a569fbfe9289f86c7f6b1fa8f09f2cdaf5c75
Russell <rkim846@gmail.com>
2020-08-10 18:02:58 -0400

    edit_room/lib

--- routes/search.js
+++ routes/search.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+	//get all schools
 	const connection = getConnection();
 	const queryString = 'SELECT * FROM schools';
 	connection.query(queryString, (err, rows, fields) => {
@@ -18,7 +19,55 @@ router.get('/', (req, res) => {
 	});
 });
 
+router.get('/building/rooms/:id', (req, res) => {
+	//get all rooms for a building
+	const connection = getConnection();
+	const id = req.params.id;
+	const queryString = 'SELECT * FROM rooms WHERE buildingId=?';
+	connection.query(queryString, [id], (err, rows, fields) => {
+		if (err) {
+			console.log('Failed to get: ' + err); //if query error
+			res.sendStatus(500);
+			return;
+		}
+		const rooms = rows.map((row) => {
+			return {
+				id: row.id,
+				name: row.title,
+				floor: row.flr,
+				num: row.num,
+				buildId: row.buildingId,
+			};
+		});
+		res.send(rooms);
+	});
+});
+
+router.get('/building/libraries/:id', (req, res) => {
+	//get all lib from school
+	const connection = getConnection();
+	const id = req.params.id;
+	const queryString = 'SELECT * FROM libraries WHERE buildingId=?';
+	connection.query(queryString, [id], (err, rows, fields) => {
+		if (err) {
+			console.log('Failed to get: ' + err); //if query error
+			res.sendStatus(500);
+			return;
+		}
+		const libraries = rows.map((row) => {
+			return {
+				id: row.id,
+				name: row.title,
+				floor: row.flr,
+				buildId: row.buildingId,
+			};
+		});
+		res.send(libraries);
+	});
+});
+
 router.get('/rooms/:id', (req, res) => {
+	//get all rooms from school
 	const connection = getConnection();
 	const id = req.params.id;
 	const queryString = 'SELECT * FROM rooms WHERE schoolId=?';
@@ -42,6 +91,7 @@ router.get('/rooms/:id', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+	//get certain school
 	const connection = getConnection();
 	const id = req.params.id;
 	const queryString = 'SELECT * FROM schools WHERE id=?';
@@ -56,6 +106,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.get('/libraries/:id', (req, res) => {
+	//get all lib from school
 	const connection = getConnection();
 	const id = req.params.id;
 	const queryString = 'SELECT * FROM libraries WHERE schoolId=?';
@@ -78,6 +129,7 @@ router.get('/libraries/:id', (req, res) => {
 });
 
 router.get('/buildings/:id', (req, res) => {
+	//get all build from school
 	const connection = getConnection();
 	const id = req.params.id;
 	const queryString = 'SELECT * FROM buildings WHERE schoolId=?';
@@ -98,6 +150,7 @@ router.get('/buildings/:id', (req, res) => {
 });
 
 router.get('/room/:id', (req, res) => {
+	//get certain rooms
 	const connection = getConnection();
 	const id = req.params.id;
 	const queryString = 'SELECT * FROM rooms WHERE id=?';
@@ -122,6 +175,7 @@ router.get('/room/:id', (req, res) => {
 });
 
 router.get('/library/:id', (req, res) => {
+	//get certain lib
 	const connection = getConnection();
 	const id = req.params.id;
 	const queryString = 'SELECT * FROM libraries WHERE id=?';
@@ -145,6 +199,7 @@ router.get('/library/:id', (req, res) => {
 });
 
 router.post('/updateroom/:id', (req, res) => {
+	//update certain room
 	//combine with below function
 	const connection = getConnection();
 	const id = req.params.id;
@@ -161,6 +216,7 @@ router.post('/updateroom/:id', (req, res) => {
 });
 
 router.post('/updatelibrary/:id', (req, res) => {
+	//update certain lib
 	const connection = getConnection();
 	const id = req.params.id;
 	const avail = req.body.avail;
